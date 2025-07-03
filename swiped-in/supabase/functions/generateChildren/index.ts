faa0e09de4d1198f771aae0fa34a2e095e12c5f4
NTh1nk <83032430+NTh1nk@users.noreply.github.com>
2025-07-03 14:18:56 -0700

    Update index.ts

--- swiped-in/supabase/functions/generateChildren/index.ts
+++ swiped-in/supabase/functions/generateChildren/index.ts
@@ -1,4 +1,4 @@
-import { createClient, SupabaseClient } from "jsr:@supabase/supabase-js@2";
+/*import { createClient, SupabaseClient } from "jsr:@supabase/supabase-js@2";
 import { Database } from "../_shared/database.types.ts";
 import { CoreMessage, GenerateObjectResult } from "npm:ai";
 import "jsr:@supabase/functions-js/edge-runtime.d.ts";
@@ -170,3 +170,4 @@ async function getPreviousScenarios(
 
   return previousScenarios;
 }
+*/
